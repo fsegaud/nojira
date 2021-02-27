f841fe9ff91dc2a32cb5ccdf8e351f2f7cd789db
François Ségaud <fsegaud@amplitude-studios.com>
2021-02-27 21:16:45 +0100

    Test program

--- nojira-test/Program.cs
+++ nojira-test/Program.cs
@@ -2,5 +2,17 @@ namespace Nojira.Test
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            Nojira.Client.RemoteLog.Init("http://localhost:1410", "nojira");
+
+            Nojira.Client.RemoteLog.LogInfo("test", "test of an info message.");
+            Nojira.Client.RemoteLog.LogWarning("test", "test of an warning message.");
+            Nojira.Client.RemoteLog.LogError("test", "test of an error message.");
+
+            Nojira.Client.RemoteLog.LogInfo("client", "Sending HTTP request...");
+            Nojira.Client.RemoteLog.LogWarning("client", "HTTP request times out (try 1/3).");
+            Nojira.Client.RemoteLog.LogError("client", "HTTP request times out (try 3/3).");
+        }
     }
 }
