f293279793055bf11aa8d678d160948490709581
François Ségaud <fsegaud@amplitude-studios.com>
2021-03-05 13:54:55 +0100

    Added basic Nancy authentication to access the web interface

---
+++ Nojira.Server/UserValidator.cs
@@ -0,0 +1,37 @@
+﻿// Copyright(c) 2021 François Ségaud
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+namespace Nojira.Server
+{
+    public class UserValidator : Nancy.Authentication.Basic.IUserValidator
+    {
+        public System.Security.Claims.ClaimsPrincipal Validate(string username, string password)
+        {
+            DB.User user = DB.SelectUser(username);
+            if (user != null && user.CheckPassword(password))
+            {
+                return new System.Security.Claims.ClaimsPrincipal(new System.Security.Principal.GenericIdentity(username));
+            }
+
+            System.Console.WriteLine($"<system> User '{username}' failed to authenticate.");
+            return null;
+        }
+    }
+}
